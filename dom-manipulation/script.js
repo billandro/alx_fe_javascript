14a437f37edcf1446ce649f2617df470889d60c4
Bill Qubeka <Bill Qubeka>
2024-07-28 22:40:09 +0200

    Implement web storage and JSON handling

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Refernce necessary elements
     const quoteDisplay = document.getElementById("quoteDisplay");
     const newQuoteBtn = document.getElementById("newQuote");
     const addQuoteToArray = document.getElementById("add-quote");
-
+    const jsonExport = document.getElementById("json-export");
+    const jsonImport = document.getElementById("importFile");
     // Object constructor
     function Quote(text, category) {
         this.text = text;
@@ -10,7 +12,39 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     // Initialize empty array
     let quotesObject = [];
-    /*let quoteToDisplay = [];*/
+    // Active random rumber
+    let activeNumber;
+    
+    // Load array from local storage on load
+    function retrieveQuotes() {
+        // Retrieve array from local storage
+        quotesObject = JSON.parse(localStorage.getItem("quotesObject") || "[]");
+        console.log(`Onload ${quotesObject}`);
+        activeNumber = JSON.parse(localStorage.getItem("activeNumber"));
+        console.log(`Active no on reload: ${activeNumber}`);
+
+        for (let i = 0; i < quotesObject.length; i++) {
+            if (i === activeNumber) {
+                console.log(`Element at index to load ${i}`);
+                const quoteCard = document.createElement("div");
+                const quoteText = document.createElement("p");
+                const quoteCategory = document.createElement("p");
+
+                quoteCard.classList.add("card");
+                quoteText.classList.add("text");
+                quoteCategory.classList.add("category");
+
+                quoteText.innerText = `"${quotesObject[i].text}"`;
+                quoteCategory.innerText = `- ${quotesObject[i].category}`;
+
+                quoteCard.appendChild(quoteText);
+                quoteCard.appendChild(quoteCategory);
+
+                quoteDisplay.appendChild(quoteCard);
+                break;
+            }
+        }
+    }
 
     // Function to add new quote to array
     function addQuote() {
@@ -24,6 +58,9 @@ document.addEventListener("DOMContentLoaded", () => {
             const createAddQuoteForm = new Quote(newQuoteText, newQuoteCategory);
             // Once created push new quote object into array
             quotesObject.push(createAddQuoteForm);
+            // Save modified array of quotes
+            localStorage.setItem("quotesObject", JSON.stringify(quotesObject));
+            console.log(`Local storage: ${localStorage}`);
             // Clear input fields
             newQuoteText = "";
             newQuoteCategory = "";
@@ -43,11 +80,15 @@ document.addEventListener("DOMContentLoaded", () => {
         if (quotesObject !== "") {
             // Choose a random number
             let randomNumber = Math.floor(Math.random() * quotesObject.length);
-            console.log(randomNumber);
+            console.log(`Random number ${randomNumber}`);
+            // Active number
+            activeNumber = randomNumber;
+            console.log(`Active number: ${activeNumber}`);
+
             /*quoteToDisplay.push(randomNumber);*/
             for (let i = 0; i < quotesObject.length; i++) {
-                console.log(i);
                 if (i === randomNumber) {
+                    console.log(`Index equal to random no is ${i}`);
                     const quoteCard = document.createElement("div");
                     const quoteText = document.createElement("p");
                     const quoteCategory = document.createElement("p");
@@ -57,12 +98,15 @@ document.addEventListener("DOMContentLoaded", () => {
                     quoteCategory.classList.add("category");
 
                     quoteText.innerText = `"${quotesObject[i].text}"`;
-                    quoteCategory.innerText = `${quotesObject[i].category}`;
+                    quoteCategory.innerText = `- ${quotesObject[i].category}`;
 
                     quoteCard.appendChild(quoteText);
                     quoteCard.appendChild(quoteCategory);
 
                     quoteDisplay.appendChild(quoteCard);
+
+                    localStorage.setItem("activeNumber", JSON.stringify(activeNumber));
+
                     break;
                 }
             }
@@ -71,9 +115,50 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    // Export quotes to JSON function
+    function JSONToFile(obj, filename) {
+        const blob = new Blob([JSON.stringify(obj, null, 2)], {
+            type: "application/json",
+        });
+
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = `${filename}.json`;
+        a.click();
+        URL.revokeObjectURL(url);
+    }
+
+    // When click export
+    jsonExport.addEventListener("click", () => {
+        quotesObject = JSON.parse(localStorage.getItem("quotesObject") || "[]");
+        console.log(`Array before export: ${quotesObject}`);
+        JSONToFile(quotesObject, "testJsonFile");
+    });
+
+    // When importing json
+    jsonImport.addEventListener("change", (event) => {
+        const fileReader = new FileReader();
+        fileReader.onload = function(event) {
+            // Parsing the loaded file
+            const importedQuotes = JSON.parse(event.target.result);
+            // Push the array of quotes objects into internal array
+            quotesObject.push(...importedQuotes);
+            // Save new combined array of quotes
+            localStorage.setItem("quotesObject", JSON.stringify(quotesObject));
+            // Alert user of success
+            alert('Quotes imported successfully!');
+        };
+        console.log(fileReader.readAsText(event.target.files[0]));
+        
+    });
+
     // When show new quote button is clicked show new quote
     newQuoteBtn.addEventListener("click", showRandomQuote);
 
     // Add new quote object to array
     addQuoteToArray.addEventListener("click", addQuote);
+
+    // Initialize quotes
+    retrieveQuotes();
 });
